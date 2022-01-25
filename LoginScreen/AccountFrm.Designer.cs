92dfa839a6fbb1805a310f479bda3d571ac14931
Matthew Walberg <mwalberg17@gmail.com>
2022-01-25 11:24:16 -0600

    saving display changes

--- LoginScreen/AccountFrm.Designer.cs
+++ LoginScreen/AccountFrm.Designer.cs
@@ -30,26 +30,95 @@ namespace LoginScreen
         private void InitializeComponent()
         {
             this.uxBackBtn = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // uxBackBtn
             // 
+            this.uxBackBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(161)))), ((int)(((byte)(255)))));
+            this.uxBackBtn.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.uxBackBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.uxBackBtn.Font = new System.Drawing.Font("Myanmar Text", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.uxBackBtn.Location = new System.Drawing.Point(288, 194);
+            this.uxBackBtn.Location = new System.Drawing.Point(13, 440);
+            this.uxBackBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.uxBackBtn.Name = "uxBackBtn";
-            this.uxBackBtn.Size = new System.Drawing.Size(96, 46);
+            this.uxBackBtn.Size = new System.Drawing.Size(554, 71);
             this.uxBackBtn.TabIndex = 1;
             this.uxBackBtn.Text = "Back";
-            this.uxBackBtn.UseVisualStyleBackColor = true;
+            this.uxBackBtn.UseVisualStyleBackColor = false;
             this.uxBackBtn.Click += new System.EventHandler(this.uxBackBtn_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(161)))), ((int)(((byte)(255)))));
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Myanmar Text", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(554, 71);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Account Information";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(161)))), ((int)(((byte)(255)))));
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Myanmar Text", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(13, 171);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(554, 71);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Manage Tickets";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(161)))), ((int)(((byte)(255)))));
+            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Myanmar Text", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(13, 92);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(554, 71);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Edit Account Information";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(161)))), ((int)(((byte)(255)))));
+            this.button4.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Myanmar Text", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(13, 250);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(554, 71);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Sports Notifications";
+            this.button4.UseVisualStyleBackColor = false;
             // 
             // AccountFrm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(189)))), ((int)(((byte)(106)))), ((int)(((byte)(230)))));
-            this.ClientSize = new System.Drawing.Size(435, 426);
+            this.ClientSize = new System.Drawing.Size(580, 524);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.uxBackBtn);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "AccountFrm";
             this.Text = "AccountFrm";
             this.ResumeLayout(false);
@@ -59,5 +128,9 @@ namespace LoginScreen
         #endregion
 
         private System.Windows.Forms.Button uxBackBtn;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
--- LoginScreen/TicketsFrm.Designer.cs
+++ LoginScreen/TicketsFrm.Designer.cs
@@ -29,35 +29,158 @@ namespace LoginScreen
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TicketsFrm));
             this.uxBackBtn = new System.Windows.Forms.Button();
+            this.uxFootballPicbox = new System.Windows.Forms.PictureBox();
+            this.uxVolleyballPicbox = new System.Windows.Forms.PictureBox();
+            this.uxBballPicbox = new System.Windows.Forms.PictureBox();
+            this.uxSoccerPicbox = new System.Windows.Forms.PictureBox();
+            this.uxFootballLabel = new System.Windows.Forms.Label();
+            this.uxVolleyballLabel = new System.Windows.Forms.Label();
+            this.uxBballLabel = new System.Windows.Forms.Label();
+            this.uxSoccerLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.uxFootballPicbox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.uxVolleyballPicbox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.uxBballPicbox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.uxSoccerPicbox)).BeginInit();
             this.SuspendLayout();
             // 
             // uxBackBtn
             // 
+            this.uxBackBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(161)))), ((int)(((byte)(255)))));
+            this.uxBackBtn.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.uxBackBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.uxBackBtn.Font = new System.Drawing.Font("Myanmar Text", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.uxBackBtn.Location = new System.Drawing.Point(12, 357);
+            this.uxBackBtn.Location = new System.Drawing.Point(10, 439);
+            this.uxBackBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.uxBackBtn.Name = "uxBackBtn";
-            this.uxBackBtn.Size = new System.Drawing.Size(411, 57);
+            this.uxBackBtn.Size = new System.Drawing.Size(560, 70);
             this.uxBackBtn.TabIndex = 1;
             this.uxBackBtn.Text = "Back";
-            this.uxBackBtn.UseVisualStyleBackColor = true;
+            this.uxBackBtn.UseVisualStyleBackColor = false;
             this.uxBackBtn.Click += new System.EventHandler(this.uxBackBtn_Click);
+            // 
+            // uxFootballPicbox
+            // 
+            this.uxFootballPicbox.Image = ((System.Drawing.Image)(resources.GetObject("uxFootballPicbox.Image")));
+            this.uxFootballPicbox.Location = new System.Drawing.Point(10, 51);
+            this.uxFootballPicbox.Name = "uxFootballPicbox";
+            this.uxFootballPicbox.Size = new System.Drawing.Size(277, 156);
+            this.uxFootballPicbox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.uxFootballPicbox.TabIndex = 2;
+            this.uxFootballPicbox.TabStop = false;
+            // 
+            // uxVolleyballPicbox
+            // 
+            this.uxVolleyballPicbox.Image = ((System.Drawing.Image)(resources.GetObject("uxVolleyballPicbox.Image")));
+            this.uxVolleyballPicbox.Location = new System.Drawing.Point(293, 51);
+            this.uxVolleyballPicbox.Name = "uxVolleyballPicbox";
+            this.uxVolleyballPicbox.Size = new System.Drawing.Size(277, 156);
+            this.uxVolleyballPicbox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.uxVolleyballPicbox.TabIndex = 3;
+            this.uxVolleyballPicbox.TabStop = false;
+            // 
+            // uxBballPicbox
+            // 
+            this.uxBballPicbox.Image = ((System.Drawing.Image)(resources.GetObject("uxBballPicbox.Image")));
+            this.uxBballPicbox.Location = new System.Drawing.Point(10, 240);
+            this.uxBballPicbox.Name = "uxBballPicbox";
+            this.uxBballPicbox.Size = new System.Drawing.Size(277, 156);
+            this.uxBballPicbox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.uxBballPicbox.TabIndex = 4;
+            this.uxBballPicbox.TabStop = false;
+            // 
+            // uxSoccerPicbox
+            // 
+            this.uxSoccerPicbox.Image = ((System.Drawing.Image)(resources.GetObject("uxSoccerPicbox.Image")));
+            this.uxSoccerPicbox.Location = new System.Drawing.Point(293, 240);
+            this.uxSoccerPicbox.Name = "uxSoccerPicbox";
+            this.uxSoccerPicbox.Size = new System.Drawing.Size(277, 156);
+            this.uxSoccerPicbox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.uxSoccerPicbox.TabIndex = 5;
+            this.uxSoccerPicbox.TabStop = false;
+            // 
+            // uxFootballLabel
+            // 
+            this.uxFootballLabel.AutoSize = true;
+            this.uxFootballLabel.BackColor = System.Drawing.Color.Transparent;
+            this.uxFootballLabel.Font = new System.Drawing.Font("Myanmar Text", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxFootballLabel.Location = new System.Drawing.Point(66, 51);
+            this.uxFootballLabel.Name = "uxFootballLabel";
+            this.uxFootballLabel.Size = new System.Drawing.Size(165, 36);
+            this.uxFootballLabel.TabIndex = 6;
+            this.uxFootballLabel.Text = "Football Tickets";
+            // 
+            // uxVolleyballLabel
+            // 
+            this.uxVolleyballLabel.AutoSize = true;
+            this.uxVolleyballLabel.BackColor = System.Drawing.Color.Transparent;
+            this.uxVolleyballLabel.Font = new System.Drawing.Font("Myanmar Text", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxVolleyballLabel.Location = new System.Drawing.Point(340, 51);
+            this.uxVolleyballLabel.Name = "uxVolleyballLabel";
+            this.uxVolleyballLabel.Size = new System.Drawing.Size(182, 36);
+            this.uxVolleyballLabel.TabIndex = 7;
+            this.uxVolleyballLabel.Text = "Volleyball Tickets";
+            // 
+            // uxBballLabel
+            // 
+            this.uxBballLabel.AutoSize = true;
+            this.uxBballLabel.BackColor = System.Drawing.Color.Transparent;
+            this.uxBballLabel.Font = new System.Drawing.Font("Myanmar Text", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxBballLabel.Location = new System.Drawing.Point(55, 240);
+            this.uxBballLabel.Name = "uxBballLabel";
+            this.uxBballLabel.Size = new System.Drawing.Size(186, 36);
+            this.uxBballLabel.TabIndex = 8;
+            this.uxBballLabel.Text = "Basketball Tickets";
+            // 
+            // uxSoccerLabel
+            // 
+            this.uxSoccerLabel.AutoSize = true;
+            this.uxSoccerLabel.BackColor = System.Drawing.Color.Transparent;
+            this.uxSoccerLabel.Font = new System.Drawing.Font("Myanmar Text", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxSoccerLabel.Location = new System.Drawing.Point(356, 240);
+            this.uxSoccerLabel.Name = "uxSoccerLabel";
+            this.uxSoccerLabel.Size = new System.Drawing.Size(150, 36);
+            this.uxSoccerLabel.TabIndex = 9;
+            this.uxSoccerLabel.Text = "Soccer Tickets";
             // 
             // TicketsFrm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(189)))), ((int)(((byte)(106)))), ((int)(((byte)(230)))));
-            this.ClientSize = new System.Drawing.Size(435, 426);
+            this.ClientSize = new System.Drawing.Size(580, 524);
+            this.Controls.Add(this.uxSoccerLabel);
+            this.Controls.Add(this.uxBballLabel);
+            this.Controls.Add(this.uxVolleyballLabel);
+            this.Controls.Add(this.uxFootballLabel);
+            this.Controls.Add(this.uxSoccerPicbox);
+            this.Controls.Add(this.uxBballPicbox);
+            this.Controls.Add(this.uxVolleyballPicbox);
             this.Controls.Add(this.uxBackBtn);
+            this.Controls.Add(this.uxFootballPicbox);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "TicketsFrm";
-            this.Text = "TicketsFrm";
+            this.Text = "Tickets";
+            ((System.ComponentModel.ISupportInitialize)(this.uxFootballPicbox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.uxVolleyballPicbox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.uxBballPicbox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.uxSoccerPicbox)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button uxBackBtn;
+        private System.Windows.Forms.PictureBox uxFootballPicbox;
+        private System.Windows.Forms.PictureBox uxVolleyballPicbox;
+        private System.Windows.Forms.PictureBox uxBballPicbox;
+        private System.Windows.Forms.PictureBox uxSoccerPicbox;
+        private System.Windows.Forms.Label uxFootballLabel;
+        private System.Windows.Forms.Label uxVolleyballLabel;
+        private System.Windows.Forms.Label uxBballLabel;
+        private System.Windows.Forms.Label uxSoccerLabel;
     }
 }
